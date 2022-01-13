d09e50287e1edcdca47c1e3fb7e60e5ec011573a
Robin Guyomar <robin.guyomar@etu.univ-lehavre.r>
2022-01-13 01:06:34 +0100

    Initialisation du projet + mise en place des voies de circulation

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', function (req, res) {
+  res.send('Hello !');
+});
+
+app.listen(3000, function () {
+  console.log("Express is running on port 3000 !");
+});
